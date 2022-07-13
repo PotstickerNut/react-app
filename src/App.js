956070bf96bf09cc8e466d14d0ff5adbb7186e44
Janette Campbell <campbjp@hotmail.com>
2022-07-12 20:56:31 -0400

    feature: added loading and error msg to GithubUser

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 const GithubUser = ({ name, location, avatar }) => {
-  console.log(avatar);
   return (
     <div>
       <h1>{name}</h1>
@@ -14,20 +13,30 @@ const GithubUser = ({ name, location, avatar }) => {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/moonhighway`)
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(setError);
   }, []);
-  if (data)
-    return (
-      <GithubUser
-        name={data.name}
-        location={data.location}
-        avatar={data.avatar_url}
-      />
-    );
-  return <h1>Data</h1>;
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
+  return (
+    <GithubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+    />
+  );
 }
 
 export default App;
